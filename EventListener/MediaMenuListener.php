cad8098534ffc035271ae6482083547f790eedcc
Paul Andrieux <paul@appventus.com>
2014-04-18 18:04:49 +0200

    add french translations + fix adminbar action

--- EventListener/MediaMenuListener.php
+++ EventListener/MediaMenuListener.php
@@ -42,22 +42,29 @@ class MediaMenuListener
      */
     public function addGlobal(Event $event)
     {
-        $mediaItem = $this->getMainItem();
-        $mediaItem->setLinkAttribute('id', 'media-manager');
 
-        return $mediaItem;
+        $this->mainItem = $this->menuBuilder->getTopNavbar();
+
+        $this->mainItem
+            ->addChild('menu.media', array(
+                'uri' => '#'
+            ))
+            ->setLinkAttribute('id', 'media-manager');
+
+        return $this->mainItem;
+
     }
 
-    private function getMainItem()
-    {
-        if ($menuMedia = $this->menuBuilder->getTopNavbar()->getChild(('menu.media'))) {
-            return $menuMedia;
-        } else {
-            return $this->menuBuilder->createDropdownMenuItem(
-                $this->menuBuilder->getTopNavBar(),
-                "menu.media",
-                array("attributes" => array( "class" => "vic-pull-left vic-text-center"))
-            );
-        }
-    }
+    // private function getMainItem()
+    // {
+    //     if ($menuMedia = $this->menuBuilder->getTopNavbar()->getChild(('menu.media'))) {
+    //         return $menuMedia;
+    //     } else {
+    //         return $this->menuBuilder->createDropdownMenuItem(
+    //             $this->menuBuilder->getTopNavBar(),
+    //             "menu.media",
+    //             array("attributes" => array( "class" => "vic-pull-left vic-text-center"))
+    //         );
+    //     }
+    // }
 }
